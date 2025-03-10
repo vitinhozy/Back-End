a92eb03bd5f44f31d81c8cebac60c0d5a0a434b7
vitinhozy <152634561+vitinhozy@users.noreply.github.com>
2025-03-10 17:43:13 -0300

    up

---
+++ PHP/speeds/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Speeds</title>
+</head>
+<body>
+    <main>
+        <h1>Cotação Dólar</h1>
+        <form action="pages/cotacaoDolar.php" method="post"></form>
+            <div class="form-input">
+                <label for="inserirCot">Inserir Cotação</label>
+                <input paltype="text">
+            </div>
+
+    </main>
+</body>
+</html>
