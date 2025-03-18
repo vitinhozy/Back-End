eb2956e6046f50e425952bb2f6582d3684a55cc0
vitinhozy <152634561+vitinhozy@users.noreply.github.com>
2025-03-18 17:46:14 -0300

    up

--- PHP/carrinho/index.php
+++ PHP/carrinho/index.php
@@ -1,19 +1,43 @@
 <?php
+    // inicia sessão
     session_start();
 
-    // Verifica se algo está null
+    class Produto{
+        public $id;
+        public $nome;
+        public $valor;
+        public $desc;
+
+        public function __construct($id, $nome, $valor, $desc){
+            $this->$id = $id;
+            $this->$nome = $nome;
+            $this->$valor = $valor;
+            $this->$desc = $desc;
+        }
+        
+    }
+
+    // public - qualquer parte do código mexe no atributo
+    // private - apenas o que está dentro de class
+
+    // Verifica se carrinho está null
     if(!isset($_SESSION['carrinho'])){
         $_SESSION['carrinho'] = [];
     }
     // adiciona produto
     if(isset($_GET['produto'])){
-        $_SESSION['carrinho'][] = $_GET['produto'];
+        $produtosDados = new Produto($_GET['id'], $_GET['produto'], $_GET['valor'], $_GET['desc']);
+        $_SESSION['carrinho'][] = $produtosDados;
+
     }
 
     if(isset($_GET['deletarCarrinho'])){
         session_destroy();
         // $_SESSION['carrinho'] = [];
     }
+
+  
+    
     // var_dump($_SESSION['carrinho']);
 ?>
 
@@ -28,25 +52,38 @@
 </head>
 <body>
     <div class="links">
-        <a href="?produto= SSD">
-            <img src="img/download.jpeg" width="80px" alt="">
-            
-            Adicionar SSD
-        </a>
-        <a href="?produto= Disco de memória">Adicionar Disco de memória</a>
-        <a href="?produto= Memória RAM">Adicionar Memória RAM</a>
-        <a href="?produto= Placa mãe">Adicionar Placa mãe</a>
+        <a href="?id=1&produto='SSD'&valor=199.00&desc='1TB de armazenamento'"><img src="img/download.jpeg" width="80px" alt="">Adicionar SSD</a>
+        <a href="?id=2&produto=Disco de memória&valor=249.90&desc='Disco Legal'">Adicionar Disco de memória</a>
+        <a href="?id=3&produto=Memória RAM&valor=180.00&desc='HyperX'">Adicionar Memória RAM</a>
+        <a href="?id=4&produto=Placa mãe&valor=120.0&desc='Barata e funcional'">Adicionar Placa mãe</a>
     </div>
     <!-- DELETAR -->
     <a href="?deletarCarrinho=True" class="delete-link">Deletar Carrinho</a>
+    
+    <?php
+        $valorTotal = 0;
 
+        foreach($_SESSION['carrinho'] as $produto){
+            $valorTotal += $produto->valor;
+        }
 
+        if($valorTotal == 0){
+            echo 'Carrinho está vazio';
+        }else{
+            echo "O valor total é R$ $valorTotal";
+        }
+    ?>
     <ol>
         <?php
             // foreach(array as alias)
             // foreach(filmes as filme) {filme.nome, filme.desc}
             foreach($_SESSION['carrinho'] as $produto){
-            echo "<li>$produto</li>";
+            echo "
+            <li>
+                <h1>$produto->nome</h1>
+                <p>$produto->valor<p>
+                <p>$produto->desc</p>
+            </li>";
             }
         ?>
     </ol>
